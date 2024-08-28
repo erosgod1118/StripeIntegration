a57c982f52c73f944ce9b12205b415e1f502ad85
Eros God <erosgod19921118@gmail.com>
2024-08-28 07:27:54 -0400

    Restructure node.js server.

--- server/app.js
+++ server/app.js
@@ -3,19 +3,21 @@
 const express = require("express");
 require('dotenv').config();
 
-const { initDB } = require("./db");
+const { initDB } = require("./model/db");
+const authRouter = require('./routes/authRoutes')
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/", require("./route"));
+app.use("/user", authRouter);
 
-const _port = 3000;
+const _port = 5000;
 
 initDB((err) => {
     if (err) throw err;
+    
     app.listen(_port, (errSrv) => {
         if (errSrv) throw errSrv;
         console.log(`Server running on port ${_port}`);
